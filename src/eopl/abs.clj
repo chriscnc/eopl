cc69d9e294bc2da3c8f1af6d5bb7e5e964050275
Chris <chris.cornelison@gmail.com>
2015-10-28 13:42:28 -0500

    Completed exercise-2.7

--- src/eopl/abs.clj
+++ src/eopl/abs.clj
@@ -1,31 +1,108 @@
 (ns eopl.abs
-  "2.2.2 Abstract Syntax and its Representation")
+  "Exercise-2.7")
 
+(defrecord LitExp [datum])
 (defrecord VarExp [id])
-(defrecord LambdaExp [id body])
-(defrecord AppExp [rator rand])
+(defrecord IfExp [test-exp true-exp false-exp])
+(defrecord LambdaExp [ids body])
+(defrecord AppExp [rator rands])
+(defrecord LexInfo [id depth position])
+(defrecord FreeInfo [id])
 
-(defn unparse-expr
-  [e]
-  (condp = (type e)
-    VarExp (:id e)
+(defn unparse-ast
+  [node]
+  (condp = (type node)
+    LitExp (:datum node)
+    VarExp (:id node)
+    IfExp (list 'if
+                (unparse-ast (:test-exp node))
+                (unparse-ast (:true-exp node))
+                (unparse-ast (:false-exp node)))
     LambdaExp (list 'lambda 
-                    (list (:id e))
-                    (unparse-expr (:body e)))
-    AppExp (list (unparse-expr (:rator e))
-                 (unparse-expr (:rand e)))
+                    (:ids node)
+                    (unparse-ast (:body node)))
+    AppExp `(~(unparse-ast (:rator node))
+              ~@(map unparse-ast (:rands node)))
     (throw (Exception. "Unmatched type"))))
-
 
 
 (defn parse-expr
   [term]
-  (cond (symbol? term) (VarExp. term)
-        (list? term) (if (= 'lambda (first term))
-                       (LambdaExp. (first (second term))
-                                   (parse-expr (nth term 2)))
-                       (AppExp. (parse-expr (first term))
-                                (parse-expr (second term))))
+  (cond (number? term) (LitExp. term)
+        (symbol? term) (VarExp. term)
+        (list? term) 
+        (condp = (first term)
+          'if (IfExp. (parse-expr (second term))
+                      (parse-expr (nth term 2))
+                      (parse-expr (nth term 3)))
+          'lambda (LambdaExp. (second term)
+                              (parse-expr (nth term 2)))
+          (AppExp. (parse-expr (first term))
+                   (map parse-expr (rest term))))
         :else (throw (Exception. (str "Invalid concrete syntax: " term)))))
 
+
+(defn get-lex-dist
+  "Retrieves the position of the first var in a list of var/positions
+  that represents a lexical environment."
+  [id lex-env]
+  (let [f (fn [id lex-env depth]
+            (when (not (empty? lex-env))
+              (let [bindings (first lex-env)]
+                (if (contains? bindings id)
+                  {:pos (get bindings id)
+                   :depth depth}
+                  (recur id (rest lex-env) (inc depth))))))
+        init-depth 0]
+    (f id lex-env init-depth)))
+
+
+
+(defn lex-addr
+  "Compute the lexical distance for all vars in an ast and emit
+  a new ast with VarExp nodes replaced with either LexInfo or FreeInfo
+  nodes."
+  [node lex-env]
+  (condp = (type node)
+    LitExp node
+    VarExp (let [id (:id node)
+                 dist (get-lex-dist id lex-env)]
+             (if dist
+               (LexInfo. id (:depth dist) (:pos dist))
+               (FreeInfo. id)))
+    IfExp (IfExp. (lex-addr (:test-exp node) lex-env)
+                  (lex-addr (:true-exp node) lex-env)
+                  (lex-addr (:false-exp node) lex-env))
+    LambdaExp (let [ids (:ids node)
+                    lex-env (cons (zipmap ids (range)) lex-env)]
+                (LambdaExp. ids
+                            (lex-addr (:body node) 
+                                      lex-env)))
+    AppExp (AppExp. (lex-addr (:rator node) lex-env)
+                    (map #(lex-addr % lex-env) (:rands node)))
+    (throw (Exception. "Unmatched type"))))
+
+
+(defn lexical-address
+  "Wrapper for the multi-arg lexical-address"
+  [ast]
+  (lex-addr ast '()))
+
+
+(defn unparse-lex-ast
+  [node]
+  (condp = (type node)
+    LitExp (:datum node)
+    FreeInfo (list (:id node) 'free)
+    LexInfo (list (:id node) (:depth node) (:position node))
+    IfExp (list 'if
+                (unparse-lex-ast (:test-exp node))
+                (unparse-lex-ast (:true-exp node))
+                (unparse-lex-ast (:false-exp node)))
+    LambdaExp (list 'lambda 
+                    (:ids node)
+                    (unparse-lex-ast (:body node)))
+    AppExp `(~(unparse-lex-ast (:rator node))
+              ~@(map unparse-lex-ast (:rands node)))
+    (throw (Exception. (str "Unmatched type" node)))))
 
--- test/eopl/abs_test.clj
+++ test/eopl/abs_test.clj
@@ -4,6 +4,30 @@
 
 
 (deftest parse-unparse
-  (let [exp '((lambda (a) (a b)) c)]
-    (is (= exp (unparse-expr (parse-expr exp))))))
+  (let [exp '((lambda (a b c) 
+                      (if (eqv? b c) (lambda (c) (cons a c))
+                        a))
+              1 2 3)]
+    (is (= exp (unparse-ast (parse-expr exp))))))
 
+
+(deftest test-get-lex-dist
+  (let [positions '({a 0} {a 1 b 2})]
+    (is (= {:pos 0 :depth 0} (get-lex-dist 'a positions)))
+    (is (= {:pos 2 :depth 1} (get-lex-dist 'b positions)))
+    (is (nil? (get-lex-dist 'c positions)))))
+
+
+(deftest test-lex-addr
+  (let [exp '((lambda (a b c) 
+                      (if (eqv? b c) 
+                        (lambda (c) (cons a c))
+                        a))
+              1 2 3)
+        lex-exp '((lambda (a b c) 
+                       (if ((eqv? free) (b 0 1) (c 0 2)) 
+                         (lambda (c) ((cons free) (a 1 0) (c 0 0)))
+                         (a 0 0)))
+               1 2 3)]
+    (is (= lex-exp (unparse-lex-ast (lexical-address (parse-expr exp)))))))
+  
